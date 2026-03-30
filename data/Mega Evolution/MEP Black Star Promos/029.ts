14af1e9c0ef4032a1cec8e1912f536dc751c4823
Kevin Vacquier <kvacquier@gmail.com>
2026-03-30 15:47:30 +0200

    Adding promos Mega Evvolution 29/30/31

---
+++ data/Mega Evolution/MEP Black Star Promos/029.ts
@@ -0,0 +1,77 @@
+import { Card } from "../../../interfaces"
+import Set from "../MEP Black Star Promos"
+
+const card: Card = {
+	set: Set,
+
+	name: {
+		en: "Mega Charizard X ex",
+		fr: "Méga-Dracaufeu X-ex",
+		de: "Mega-Glurak X-ex",
+		it: "Mega Charizard X-ex",
+		es: "Mega-Charizard X ex",
+		pt: "Mega Charizard X ex",
+		'es-mx': "Mega-Charizard X ex",
+	},
+
+	suffix: "EX",
+	illustrator: "takuyoa",
+	rarity: "None",
+	category: "Pokemon",
+	hp: 360,
+	types: ["Fire"],
+	stage: "Stage2",
+	dexId: [10087],
+
+	attacks: [
+	{
+		cost: ["Fire", "Fire"],
+
+		name: {
+			en: "Inferno X",
+			fr: "Inferno X",
+			de: "Inferno X",
+			it: "Inferno X",
+			es: "Infierno X",
+			pt: "Inferno X",
+			'es-mx': "Infierno X",
+		},
+
+		damage: "90×",
+
+		effect: {
+			en: "Discard any amount of {R} Energy from among your Pokémon, and this attack does 90 damage for each card you discarded in this way.",
+			fr: "Défaussez autant d'Énergies {R} que vous le voulez parmi vos Pokémon. Cette attaque inflige 90 dégâts pour chaque carte défaussée de cette façon.",
+			de: "Lege beliebig viele {R}-Energien von deinen Pokémon auf deinen Ablagestapel, und diese Attacke fügt für jede Karte, die du auf diese Weise auf den Ablagestapel gelegt hast, 90 Schadenspunkte zu.",
+			it: "Scarta un numero qualsiasi di Energie {R} dai tuoi Pokémon e questo attacco infligge 90 danni per ogni carta che hai scartato in questo modo.",
+			es: "Descarta cualquier cantidad de Energías {R} de tus Pokémon, y este ataque hace 90 puntos de daño por cada carta que hayas descartado de esta manera.",
+			pt: "Descarte qualquer quantidade de Energia {R} dentre seus Pokémon, e este ataque causa 90 pontos de dano para cada carta descartada desta forma.",
+			'es-mx': "Descarta cualquier cantidad de Energías {R} de tus Pokémon, y este ataque hace 90 puntos de daño por cada carta que hayas descartado de esta manera.",
+		},
+	},
+	],
+
+	evolveFrom: {
+		en: "Charmeleon",
+		fr: "Reptincel",
+		de: "Glutexo",
+		it: "Charmeleon",
+		es: "Charmeleon",
+		pt: "Charmeleon"
+	},
+
+	retreat: 2,
+	regulationMark: "I",
+
+	thirdParty: {
+		tcgplayer: 680639,
+	},
+
+	variants: [
+	{
+		type: "holo",
+	}
+	]
+}
+
+export default card
---
+++ data/Mega Evolution/MEP Black Star Promos/031.ts
@@ -0,0 +1,96 @@
+import { Card } from "../../../interfaces"
+import Set from "../MEP Black Star Promos"
+
+const card: Card = {
+	set: Set,
+
+	name: {
+		en: "N's Zekrom",
+		fr: "Zekrom de N",
+		de: "Ns Zekrom",
+		it: "Zekrom di N",
+		es: "Zekrom de N",
+		pt: "Zekrom do N",
+		'es-mx': "Zekrom de N",
+	},
+
+	illustrator: "Bun Toujo",
+	rarity: "None",
+	category: "Pokemon",
+	hp: 130,
+	types: ["Dragon"],
+	stage: "Basic",
+	dexId: [644],
+
+	attacks: [
+	{
+		cost: ["Colorless", "Colorless", "Colorless"],
+
+		name: {
+			en: "Shred",
+			fr: "Déchiquetage",
+			de: "Zerfetzer",
+			it: "Tritatutto",
+			es: "Hacer Trizas",
+			pt: "Triturar",
+			'es-mx': "Hacer Trizas",
+		},
+
+		damage: 70,
+
+		effect: {
+			en: "This attack's damage isn't affected by any effects on your opponent's Active Pokémon.",
+			fr: "Les dégâts de cette attaque ne sont affectés par aucun effet en action sur le Pokémon Actif de votre adversaire.",
+			de: "Der Schaden dieser Attacke wird durch Effekte auf dem Aktiven Pokémon deines Gegners nicht verändert.",
+			it: "I danni di questo attacco non sono influenzati da alcun effetto presente sul Pokémon attivo del tuo avversario.",
+			es: "El daño de este ataque no se ve afectado por ningún efecto en el Pokémon Activo de tu rival.",
+			pt: "O dano deste ataque não é afetado por quaisquer efeitos no Pokémon Ativo do seu oponente.",
+			'es-mx': "El daño de este ataque no se ve afectado por ningún efecto en el Pokémon Activo de tu rival.",
+		},
+	},
+	{
+		cost: ["Fire", "Lightning", "Lightning", "Colorless"],
+
+		name: {
+			en: "Rampaging Thunder",
+			fr: "Tonnerre Saccageur",
+			de: "Wütender Donner",
+			it: "Tuono Impazzito",
+			es: "Furia Trueno",
+			pt: "Trovão Voraz",
+			'es-mx': "Furia Trueno",
+		},
+
+		damage: 250,
+
+		effect: {
+			en: "During your next turn, this Pokémon can't use attacks.",
+			fr: "Pendant votre prochain tour, ce Pokémon ne peut pas utiliser d'attaques.",
+			de: "Während deines nächsten Zuges kann dieses Pokémon keine Attacken einsetzen.",
+			it: "Durante il tuo prossimo turno, questo Pokémon non può usare attacchi.",
+			es: "Durante tu próximo turno, este Pokémon no puede usar ataques.",
+			pt: "Durante o seu próximo turno, este Pokémon não poderá usar ataques.",
+			'es-mx': "Durante tu próximo turno, este Pokémon no puede usar ataques.",
+		},
+	},
+	],
+
+	retreat: 2,
+	regulationMark: "I",
+
+	thirdParty: {
+		tcgplayer: 680480,
+	},
+
+	variants: [
+	{
+		type: "holo",
+	},
+	{
+		type: "holo",
+		stamp: ["pokemon-center"]
+	},
+	]
+}
+
+export default card

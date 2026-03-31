c5b0d363d4396536435c98e75481313cd50e2c67
Kevin Vacquier <kvacquier@gmail.com>
2026-03-31 10:52:39 +0200

    WIP 2

---
+++ data/Mega Evolution/Perfect Order.ts
@@ -0,0 +1,35 @@
+import { Set } from '../../interfaces'
+import serie from '../Mega Evolution'
+
+const set: Set = {
+	id: "me03",
+
+	name: {
+		de: "Optimale Ordnung",
+		en: "Perfect Order",
+		es: "Equilibrio Perfecto",
+		'es-mx': "Equilibrio Perfecto",
+		fr: "Equilibre Parfait",
+		it: "Equilibrio Perfecto",
+		pt: "Equilíbrio Perfeito"
+	},
+
+	serie: serie,
+
+	cardCount: {
+		official: 124
+	},
+
+	releaseDate: "2026-03-28",
+
+	abbreviations: {
+		official: "POR"
+	},
+
+	thirdParty: {
+		cardmarket: 6299,
+		tcgplayer: 24448
+	}
+}
+
+export default set
